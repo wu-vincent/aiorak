6de02c3ac35d6e54d4ec5c5425bcae46787774a9
Vincent <magicdroidx@gmail.com>
2025-07-03 15:05:28 +0100

    feat: add basic send function

--- src/aiorak/reliability.py
+++ src/aiorak/reliability.py
@@ -1,4 +1,12 @@
+import asyncio
 import enum
+import typing
+from collections import deque
+from dataclasses import dataclass
+
+from .constants import NUMBER_OF_ORDERED_STREAMS
+from .sliding_window import SlidingWindow
+from . import constants
 
 
 class Reliability(enum.IntEnum):
@@ -18,7 +26,7 @@ class Reliability(enum.IntEnum):
 
     @property
     def is_sequenced(self) -> bool:
-        return self in {UNRELIABLE_SEQUENCED, Reliability.RELIABLE_SEQUENCED}
+        return self in {Reliability.UNRELIABLE_SEQUENCED, Reliability.RELIABLE_SEQUENCED}
 
     @property
     def is_ordered(self) -> bool:
@@ -43,23 +51,43 @@ class Reliability(enum.IntEnum):
 
 @dataclass(slots=True)
 class InternalPacket:
+    HEADER_SIZE: typing.ClassVar[int] = 23  # worse case scenario
     data: bytes
     reliability: Reliability
-    reliable_index: int | None = None
-    ordering_index: int | None = None
-    sequencing_index: int | None = None
+    reliable_id: int | None = None
+    ordering_id: int | None = None
+    sequencing_id: int | None = None
     channel: int = 0
     split_id: int | None = None
     split_index: int | None = None
     split_count: int | None = None
 
 
+@dataclass(slots=True)
+class Datagram:
+    HEADER_SIZE: typing.ClassVar[int] = 4  # flag (1 byte) + datagram_id (3 bytes)
+    is_valid: bool
+    is_ack: bool
+    is_nak: bool
+    is_continuous_send: bool
+    datagram_id: int
+    payloads: list[InternalPacket]
+
+
 class ReliabilityLayer:
-    def __init__(self, mtu: int = 576):
-        self._mtu = max(mtu, 576)
+    def __init__(self, mtu: int):
+        assert mtu <= constants.MAXIMUM_MTU_SIZE, "MTU is too large"
+        self.loop = asyncio.get_event_loop()
+        self._cc = SlidingWindow(mtu - constants.UDP_HEADER_SIZE)
+        self._send_queue: deque[InternalPacket] = deque()
+        self._resend_queue: deque[InternalPacket] = deque()  # TODO: find a better data structure
+        self._ordered_ids = [0] * NUMBER_OF_ORDERED_STREAMS
+        self._sequenced_ids = [0] * NUMBER_OF_ORDERED_STREAMS
+        self._flush_handle: asyncio.TimerHandle | None = None
 
     def send(
         self,
+        transport: asyncio.DatagramTransport,
         data: bytes | memoryview,
         *,
         reliable: bool,
@@ -67,14 +95,63 @@ class ReliabilityLayer:
         sequenced: bool = False,
         channel: int = 0,
     ) -> None:
-        pass
+        assert 0 <= channel < constants.NUMBER_OF_ORDERED_STREAMS, "Invalid channel"
+        assert len(data) > 0, "Data is empty"
 
-    def next_reliable_index(self) -> int:
-        idx = self.reliable_index
-        self.reliable_index += 1
-        return idx
+        if len(data) > self._cc.max_mtu - Datagram.HEADER_SIZE - InternalPacket.HEADER_SIZE:
+            self._send_split(transport, data, ordered=ordered, sequenced=sequenced, channel=channel)
+            return
 
-    def next_sequence_number(self) -> int:
-        seq = self.sequence_number
-        self.sequence_number += 1
-        return seq
+        self._send_message(transport, data, reliable=reliable, ordered=ordered, sequenced=sequenced, channel=channel)
+
+    def _send_message(
+        self,
+        transport: asyncio.DatagramTransport,
+        data: bytes | memoryview,
+        *,
+        reliable: bool,
+        ordered: bool = False,
+        sequenced: bool = False,
+        channel: int = 0,
+    ):
+        message = InternalPacket(
+            data=data, reliability=Reliability.from_flags(reliable, ordered, sequenced), channel=channel
+        )
+        if sequenced:
+            message.ordering_id = self._ordered_ids[channel]
+            message.sequencing_id = self._next_sequencing_id(channel)
+        elif ordered:
+            message.ordering_id = self._next_ordering_id(channel)
+            self._sequenced_ids[channel] = 0
+
+        self._send_queue.append(message)
+
+        if self._flush_handle is None:
+            self._flush_handle = self.loop.call_later(0.01, self.flush, transport)
+
+    def _send_split(
+        self,
+        transport: asyncio.DatagramTransport,
+        data: bytes | memoryview,
+        *,
+        ordered: bool = False,
+        sequenced: bool = False,
+        channel: int = 0,
+    ) -> None:
+        raise NotImplementedError
+
+    def _next_ordering_id(self, channel: int) -> int:
+        ordering_id = self._ordered_ids[channel]
+        self._ordered_ids[channel] = (self._ordered_ids[channel] + 1) & 0xFFFFFF
+        return ordering_id
+
+    def _next_sequencing_id(self, channel: int) -> int:
+        sequencing_id = self._sequenced_ids[channel]
+        self._sequenced_ids[channel] = (self._sequenced_ids[channel] + 1) & 0xFFFFFF
+        return sequencing_id
+
+    def flush(self, transport: asyncio.DatagramTransport) -> None:
+        self._flush_handle = None
+
+        if not self._send_queue and not self._resend_queue:
+            return
